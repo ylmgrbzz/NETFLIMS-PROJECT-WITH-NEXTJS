c143e71f30cd2c88ab9b8a166230f18c173904df
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-08-29 18:44:42 +0300

    chore: Add categories component and update home container

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Movies from "@/mocks/movies.json";
+import Genres from "@/mocks/genres.json";
 import { FeaturedMovie } from "@/components/featueed-movie";
+import { Categories } from "@/components/categories";
 
 const HomeContainer = () => {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
+      <Categories categories={Genres?.genres.slice(0, 5)} />
     </div>
   );
 };
