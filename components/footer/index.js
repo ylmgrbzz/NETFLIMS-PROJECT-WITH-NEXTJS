385e5c6269f7f104f90bf6659c41049165aafeb9
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-08-29 18:26:30 +0300

    chore: Update header and footer components, improve layout and styles

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
+import styles from "./styles.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp;
+      <Link href="https://twitter.com/ylmgrbzz" target="_blank">
+        Yalım Gürbüz
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
