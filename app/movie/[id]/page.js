881a0c3ae92d8d0f2ce50681d9bbc7a68af565b7
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-08-29 19:16:32 +0300

    chore: Update movie page to include delay before fetching movie details

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -5,7 +5,14 @@ import Movies from "@/mocks/movies.json";
 
 // import { fetchSingleMovie } from "@/services/movie";
 
+async function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function MoviePage({ params, searchParams }) {
+  await delay(5000);
   console.log(params);
   // const movieDetail = await fetchSingleMovie(params.id);
 
